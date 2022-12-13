18d342616b2dc6933ac50cde69a9e0e767ae360b
Sam Black <sam.black@lubbockisd.org>
2022-12-13 13:38:51 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    public static void main(String[] args) throws Throwable {
+
+        partOne();
+        partTwo();
+
+    }
+
+    private static void partTwo() throws Throwable {
+        Scanner infile = new Scanner(new File("test.dat"));
+        
+    }
+
+    private static void partOne() throws Throwable {
+        Scanner infile = new Scanner(new File("test.dat"));
+
+    }
+}
