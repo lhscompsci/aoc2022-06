2517dc1108e6320cbef5e6ff84ce4945b79ebad6
Sam Black <sam.black@lubbockisd.org>
2022-12-15 14:11:44 -0600

    starting part 1

--- src/Main.java
+++ src/Main.java
@@ -20,12 +20,29 @@ public class Main {
     private static void partOne() throws Throwable {
         Scanner infile = new Scanner(new File("basic.dat"));
 
+        int answer = -1;
         while(infile.hasNext()) {
-            String blast = infile.next();
+            String blast = infile.nextLine();
             char[] burst = blast.toCharArray();
+            ArrayList<Character> group = new ArrayList<>();
+            boolean dupes = false;
+            group.add(burst[0]);
+            group.add(burst[1]);
+            group.add(burst[2]);
 
-
+            for(int x = 3; x < burst.length; x++){
+                if(!group.contains(burst[x]))
+                    dupes = true;
+                group.add(burst[x]);
+                if(dupes) {
+                    answer = x;
+                    break;
+                }
+                group.remove(0);
+            }
+            out.println(answer);
         }
+
 
     }
 }
